2f8fe2f6c7033871c52ad14961853869efd2316a
Mavrin <Andrew.2209@yandex.ru>
2023-04-30 21:27:37 +0300

    вторая итерация

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,22 @@
-let buttonOpenPopap = document.querySelector('.profile__edit-btn');
-let buttonClosePopap = document.querySelector('.popup__close-btn');
-let popap = document.querySelector('.popup');
+let buttonOpenPopup = document.querySelector('.profile__edit-btn');
+let buttonClosePopup = document.querySelector('.popup__close-btn');
+let popup = document.querySelector('.popup');
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__form-item_type_name');
 let jobInput = document.querySelector('.popup__form-item_type_job');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubTitle = document.querySelector('.profile__subtitle');
 
-let PopupFunction = function () {
-  popap.classList.toggle('popup_opened');
+let openPopup = function () {
+  popup.classList.add('popup_opened');
+
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubTitle.textContent;
 }
 
-buttonOpenPopap.addEventListener('click', PopupFunction);
-buttonClosePopap.addEventListener('click', PopupFunction);
-
-nameInput.value = profileTitle.textContent;
-jobInput.value = profileSubTitle.textContent;
+let closePopup = function () {
+  popup.classList.remove('popup_opened');
+}
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
@@ -23,7 +24,9 @@ function handleFormSubmit(evt) {
   profileTitle.textContent = `${nameInput.value}`;
   profileSubTitle.textContent = `${jobInput.value}`;
 
-  PopupFunction();
+  closePopup();
 }
 
+buttonOpenPopup.addEventListener('click', openPopup);
+buttonClosePopup.addEventListener('click', closePopup);
 formElement.addEventListener('submit', handleFormSubmit);
