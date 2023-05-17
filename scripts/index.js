56b97576b2685ede681bb1ee23eb75f14c2be2be
Mavrin <Andrew.2209@yandex.ru>
2023-05-17 12:02:53 +0300

    первая итерация

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,90 @@
-let buttonOpenPopup = document.querySelector('.profile__edit-btn');
-let buttonClosePopup = document.querySelector('.popup__close-btn');
-let popup = document.querySelector('.popup');
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__form-item_type_name');
-let jobInput = document.querySelector('.popup__form-item_type_job');
-let profileTitle = document.querySelector('.profile__title');
-let profileSubTitle = document.querySelector('.profile__subtitle');
+const elementsContainer = document.querySelector('.elements');
+const buttonEditProfile = document.querySelector('.profile__edit-btn');
+const buttonAddProfile = document.querySelector('.profile__add-btn');
+const buttonClosePopups = document.querySelectorAll('.popup__close-btn');
+const elementTemplate = document.querySelector('#element').content;
+const elementCard = elementTemplate.querySelector('.element');
+const popupTypeEdit = document.querySelector('.popup_type_edit');
+const popupTypeCreate = document.querySelector('.popup_type_create');
+const popupTypeImage = document.querySelector('.popup_type_image');
+const popupImage = document.querySelector('.popup__figure-image');
+const popupCaption = document.querySelector('.popup__caption');
+const formElementTypeEdit = document.querySelector('.popup__form_type_edit');
+const formElementTypeCreate = document.querySelector('.popup__form_type_create');
+const nameInputTypeEdit = document.querySelector('.popup__form-item_type_name');
+const nameInputTypeCreate = document.querySelector('.popup__form-item_type_name-place');
+const jobInput = document.querySelector('.popup__form-item_type_job');
+const urlInput = document.querySelector('.popup__form-item_type_url');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubTitle = document.querySelector('.profile__subtitle');
 
-let openPopup = function () {
-  popup.classList.add('popup_opened');
+function openPopup(item) {
+  item.classList.add('popup_opened');
+};
 
-  nameInput.value = profileTitle.textContent;
-  jobInput.value = profileSubTitle.textContent;
-}
+function closePopup(item) {
+  item.classList.remove('popup_opened');
+};
 
-let closePopup = function () {
-  popup.classList.remove('popup_opened');
-}
+function createElement(element) {
+  const cardCreate = elementCard.cloneNode(true);
+  const elementImage = cardCreate.querySelector('.element__image');
+  const elementTitle = cardCreate.querySelector('.element__title');
+  elementTitle.textContent = element.name;
+  elementImage.src = element.link;
+  elementImage.alt = element.name;
+  cardCreate.querySelector('.element__like-icon-btn').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('element__like-icon-btn_active');
+  });
+  cardCreate.querySelector('.element__delete-icon-btn').addEventListener('click', function (evt) {
+    evt.target.closest('.element').remove();
+  });
+  elementImage.addEventListener('click', function () {
+    openPopup(popupTypeImage);
+    popupImage.src = elementImage.src;
+    popupImage.alt = elementImage.alt;
+    popupCaption.textContent = elementTitle.textContent;
+  });
+  return cardCreate;
+};
+
+function addElement(cardAdd) {
+  elementsContainer.append(cardAdd);
+};
+
+initialCards.forEach(element => {
+  const cardAdd = createElement(element);
+  addElement(cardAdd);
+});
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
+  profileTitle.textContent = nameInputTypeEdit.value;
+  profileSubTitle.textContent = jobInput.value;
+  closePopup(popupTypeEdit);
+};
 
-  profileTitle.textContent = `${nameInput.value}`;
-  profileSubTitle.textContent = `${jobInput.value}`;
+function formSendHandler(evt) {
+  evt.preventDefault();
+  elementsContainer.prepend(createElement({ name: nameInputTypeCreate.value, link: urlInput.value }));
+  nameInputTypeCreate.value = "";
+  urlInput.value = "";
+  closePopup(popupTypeCreate);
+};
 
-  closePopup();
-}
+buttonClosePopups.forEach(element => {
+  element.addEventListener('click', function (evt) {
+    evt.target.closest('.popup').classList.remove('popup_opened');
+  });
+});
 
-buttonOpenPopup.addEventListener('click', openPopup);
-buttonClosePopup.addEventListener('click', closePopup);
-formElement.addEventListener('submit', handleFormSubmit);
+buttonEditProfile.addEventListener('click', function () {
+  openPopup(popupTypeEdit);
+  nameInputTypeEdit.value = profileTitle.textContent;
+  jobInput.value = profileSubTitle.textContent;
+});
+buttonAddProfile.addEventListener('click', function () {
+  openPopup(popupTypeCreate);
+});
+formElementTypeEdit.addEventListener('submit', handleFormSubmit);
+formElementTypeCreate.addEventListener('submit', formSendHandler);
---
+++ scripts/initialCards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: 'Лягушка',
+    link: 'https://images.unsplash.com/photo-1682407853472-78554e927bf6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+  },
+  {
+    name: 'Королевский пингвин',
+    link: 'https://images.unsplash.com/photo-1681459798045-de729f4e59b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1622&q=80'
+  },
+  {
+    name: 'Фламинго',
+    link: 'https://images.unsplash.com/photo-1682463927445-02cf23d07640?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80'
+  },
+  {
+    name: 'Селезень',
+    link: 'https://images.unsplash.com/photo-1681584157412-085782d563fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+  },
+  {
+    name: 'Носорог',
+    link: 'https://images.unsplash.com/photo-1656836678116-c6766b8b3d8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+  },
+  {
+    name: 'Большая ящерица',
+    link: 'https://images.unsplash.com/photo-1682463927610-f02e17fe7838?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+  }
+];
